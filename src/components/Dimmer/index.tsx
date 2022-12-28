d520413e15dc840a16372d398176dba34e458d78
yondav <yoni@yondav.us>
2022-12-28 14:59:44 -0500

    style(components): modifies dimmer component
    all cosmetic

--- src/components/Dimmer/index.tsx
+++ src/components/Dimmer/index.tsx
@@ -8,16 +8,25 @@ import type { DimmerProps } from 'types';
 
 const { dimmer } = classes;
 
-const Dimmer = styled(({ ...props }: { active?: boolean; onClick?: () => void }) => (
-  <motion.div
-    variants={{ hidden: { opacity: 0 }, visible: { opacity: 1 } }}
-    initial='hidden'
-    animate='visible'
-    exit='hidden'
-    transition={{ duration: 0.6, delay: 0.2, ease: 'easeOut' }}
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    {...(({ active, ...rest }) => rest)(props)}
-  />
-))<DimmerProps>(({ active }) => dimmer({ active }));
+const Dimmer = styled(
+  ({
+    animate = true,
+    ...props
+  }: {
+    active?: boolean;
+    onClick?: () => void;
+    animate?: boolean;
+  }) => (
+    <motion.div
+      variants={{ hidden: { opacity: 0 }, visible: { opacity: 1 } }}
+      initial={animate ? 'hidden' : 'visible'}
+      animate='visible'
+      exit='hidden'
+      transition={{ duration: 0.6, delay: 0.2, ease: 'easeOut' }}
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      {...(({ active, ...rest }) => rest)(props)}
+    />
+  )
+)<DimmerProps>(({ active }) => dimmer({ active }));
 
 export default Dimmer;
