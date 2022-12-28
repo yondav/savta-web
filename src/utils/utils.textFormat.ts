6b695e57036e5db1a8d08023f8459bbc3e094e9c
yondav <yoni@yondav.us>
2022-12-28 14:55:50 -0500

    feat(utils): adds formatting function from slug and to sentence

--- src/utils/utils.textFormat.ts
+++ src/utils/utils.textFormat.ts
@@ -1,9 +1,8 @@
-function toTitleCase(str: string) {
-  return str
+const toTitleCase = (str: string) =>
+  str
     .split(/[\s,|-|_]+/)
     .map(word => word.charAt(0).toUpperCase() + word.slice(1))
     .join(' ');
-}
 
 const toSlug = (str: string) =>
   str
@@ -13,4 +12,12 @@ const toSlug = (str: string) =>
     .replace(/[\s_-]+/g, '-')
     .replace(/^-+|-+$/g, '');
 
-export { toTitleCase, toSlug };
+const fromSlug = (str: string) => str.toLowerCase().trim().replace(/-/g, ' ').trim();
+
+const toSentence = (str: string) =>
+  str
+    .split(/[\s,|-|_]+/)
+    .map((word, i) => (i === 0 ? word.charAt(0).toUpperCase() + word.slice(1) : word))
+    .join(' ');
+
+export { toTitleCase, toSlug, fromSlug, toSentence };
