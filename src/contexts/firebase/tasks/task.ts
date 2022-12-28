60520ba2e8f64f3ab311a9b5e9dc5a4e2897be6e
yondav <yoni@yondav.us>
2022-12-28 15:05:13 -0500

    feat(contexts): uppermost constructor for firebase task functionality

---
+++ src/contexts/firebase/tasks/task.ts
@@ -0,0 +1,73 @@
+import type {
+  DocumentData,
+  FirestoreError,
+  QueryDocumentSnapshot,
+  SnapshotOptions,
+} from 'firebase/firestore';
+
+import { fromSlug, toSentence } from 'utils/utils.textFormat';
+
+interface TaskConfig<T> {
+  coll: string;
+  doc?: string;
+  data?: T;
+}
+
+export type ReturnType<T, A = void> = {
+  message: string;
+  error: FirestoreError | null;
+  data: (A extends [] ? T[] : T) | null;
+};
+
+export default class FirebaseTask<T extends Record<string, any>> {
+  public collName: string;
+
+  public docName?: string;
+
+  public data?: T;
+
+  constructor(config: TaskConfig<T>) {
+    this.collName = config.coll;
+    this.docName = config.doc;
+    this.data = config.data;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  protected converter() {
+    return {
+      toFirestore: (data: T): DocumentData => ({ ...data }),
+      fromFirestore: (snap: QueryDocumentSnapshot, options: SnapshotOptions): T => {
+        const data = snap.data(options)!;
+        return data as T;
+      },
+    };
+  }
+
+  protected response<A = void>(
+    data: (A extends [] ? T[] : T) | null = null,
+    err: unknown = null,
+    message: string | null = null
+  ): ReturnType<T, A> {
+    const error = err as FirestoreError;
+
+    if (!this.collName)
+      return {
+        message: 'Insufficient input for query - abort',
+        error: null,
+        data: null,
+      };
+
+    if (error)
+      return {
+        message: toSentence(fromSlug(error.code.split('/').slice(1).join())),
+        error,
+        data,
+      };
+
+    return {
+      message: message || 'success',
+      error,
+      data,
+    };
+  }
+}
