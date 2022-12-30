83eacccc5ad95c493c01350e2c35eb5c1a3badc9
yondav <yoni@yondav.us>
2022-12-29 20:42:01 -0500

    feat(App): adds light animation for route changes in auth routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,32 @@
+import { AnimatePresence } from 'framer-motion';
 import React from 'react';
-import { Outlet, Route, Routes } from 'react-router-dom';
+import { Outlet, Route, Routes, useLocation } from 'react-router-dom';
 
 import ToastProvider from 'contexts/toast';
 import { Layout, Home, Auth } from 'pages';
 import AuthProvider from 'contexts/firebase/auth';
 
 function App() {
+  const location = useLocation();
   return (
     <ToastProvider>
       <AuthProvider>
-        <Routes>
-          <Route path='auth/' element={<Auth.Layout />}>
-            <Route path='signup' element={<Auth.Signup />} />
-            <Route path='login' element={<Auth.Login />} />
-            <Route path='forgot-password' element={<Auth.ForgotPassword />} />
-            <Route path=':userId' element={<Outlet />}>
-              <Route path='dashboard' element={<Auth.Dashboard />} />
+        <AnimatePresence mode='wait'>
+          <Routes location={location} key={location.pathname}>
+            <Route path='auth/' element={<Auth.Layout />}>
+              <Route path='signup' element={<Auth.Signup />} />
+              <Route path='login' element={<Auth.Login />} />
+              <Route path='forgot-password' element={<Auth.ForgotPassword />} />
+              <Route path=':userId' element={<Outlet />}>
+                <Route path='dashboard' element={<Auth.Dashboard />} />
+              </Route>
             </Route>
-          </Route>
-          <Route path='/' element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path='*' element={<div>404</div>} />
-          </Route>
-        </Routes>
+            <Route path='/:userId' element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path='*' element={<div>404</div>} />
+            </Route>
+          </Routes>
+        </AnimatePresence>
       </AuthProvider>
     </ToastProvider>
   );
--- src/pages/Auth/Layout.tsx
+++ src/pages/Auth/Layout.tsx
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion';
 import React, { useEffect, useState } from 'react';
 import { Outlet, useLocation, useParams } from 'react-router-dom';
 import tw, { styled } from 'twin.macro';
@@ -13,12 +14,21 @@ enum AuthRoutes {
   dashboard = 'dashboard',
 }
 
-const Wrapper = styled(Container)(
-  tw`bottom-28 mx-auto w-full h-full min-h-screen bg-neutral-400 rounded-none bg-center bg-cover bg-no-repeat saturate-150`
+const Wrapper = styled(motion.div).attrs(() => ({
+  variants: {
+    hidden: { filter: 'saturate(100%)' },
+    visible: { filter: 'saturate(150%)' },
+  },
+  initial: 'hidden',
+  animate: 'visible',
+  exit: 'hidden',
+  transition: { duration: 0.5, ease: 'linear' },
+}))(
+  tw`flex justify-center items-center bottom-28 mx-auto w-full h-full min-h-screen bg-neutral-400 rounded-none bg-center bg-cover bg-no-repeat saturate-150`
 );
 
 const Card = styled(Container)<{ path: AuthRoutes }>(({ path }) => [
-  tw`w-full bg-neutral-100 rounded-lg shadow-2xl`,
+  tw`w-full bg-neutral-50 rounded-lg shadow-2xl transition-all`,
   path === AuthRoutes.signup && tw`max-w-[600px]`,
   (path === AuthRoutes.login || path === AuthRoutes.forgot) && tw`max-w-[400px]`,
   path === AuthRoutes.dashboard && tw`max-w-[1024px]`,
@@ -45,7 +55,7 @@ export default function Layout() {
 
   return (
     <Wrapper
-      flex={{ justifyContent: 'center', alignItems: 'center' }}
+      // flex={{ justifyContent: 'center', alignItems: 'center' }}
       style={{ backgroundImage: `url(${bg})` }}
     >
       <Card path={path}>
